30ea184c0b2d5b1abc835b0ac1f3cf0d29493c3e
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-08-07 19:26:40 -0400

    disable :Lazy notifications on weekdays

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -4,11 +4,18 @@ local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
 if not vim.uv.fs_stat(lazy_path) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazy_path })
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazy_path,
+    })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
+            { out, "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -21,7 +28,12 @@ vim.opt.rtp:prepend(lazy_path)
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
+local weekday = os.date("*t").wday
+
 require("lazy").setup({
     spec = { import = "plugins" },
-    checker = { enabled = true },
+    checker = {
+        -- I'm only allowed to edit my neovim configs on the weekend
+        enabled = weekday == 1 or weekday == 7,
+    },
 })
