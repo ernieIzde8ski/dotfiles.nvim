b20f1f2ac7d8c45c26b94e6914f41a5b14ae3643
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-08-11 18:45:39 -0400

    Extend insert mode `Tab` keymap and start `<Leader>bD` keymap
    The `<Tab>` and `<S-Tab>` keymaps work perfectly. Tab to jump to next
    snippet sequence when editing a snippet. Nice and easy. Dubious API, but
    I don't really care.
    
    The `<Leader>bD` keymap is more dubious. Starting to work on resolving
    \#3, but I'm not quite there yet---the new function doesn't close
    `:help` buffers and seems to throw errors sometimes, so I'm leaving it
    as an alternate binding for now.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,6 +6,29 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
     end,
 })
 
+-- local notify = require("notify")
+local write_all_buffers = vim.cmd.wall
+local close_buffer = vim.cmd.bd
+
+local function close_other_buffers()
+    write_all_buffers()
+
+    local current_bufnr = vim.api.nvim_get_current_buf()
+    local active_bufnrs = vim.tbl_filter(function(bufnr)
+        return bufnr ~= current_bufnr and vim.api.nvim_buf_is_valid(bufnr)
+        -- and vim.api.nvim_get_option_value("buflisted", { buf = bufnr })
+    end, vim.api.nvim_list_bufs())
+
+    for _, bufnr in ipairs(active_bufnrs) do
+        if bufnr ~= current_bufnr then
+            close_buffer(bufnr)
+        end
+    end
+
+    vim.notify("Closed " .. #active_bufnrs .. " buffers")
+end
+set_keymap("n", "<Leader>bD", close_other_buffers)
+
 -- close other buffers
 set_keymap("n", "<Leader>bd", "mc<cmd>wall | %bd | e# | bd#<cr>`c")
 
@@ -23,3 +46,22 @@ set_keymap("n", "<F5>", vim.cmd.update)
 set_keymap("n", "<F6>", "<cmd>!%:p<cr>") -- execute current file
 set_keymap({ "n", "v" }, "gm", "gM")
 set_keymap({ "n", "v" }, "gM", "gm")
+
+---jump with `key` to snippet in `direction`, if available
+---@param key string
+---@param direction integer
+---@return fun(): string
+local function jump_with(key, direction)
+    local command = "<cmd>lua vim.snippet.jump(" .. direction .. ")<cr>"
+    local function resp()
+        if vim.snippet.active({ direction = direction }) then
+            return command
+        else
+            return key
+        end
+    end
+    return resp
+end
+
+vim.keymap.set({ "i", "s" }, "<Tab>", jump_with("<Tab>", 1), { expr = true })
+vim.keymap.set({ "i", "s" }, "<S-Tab>", jump_with("<S-Tab>", -1), { expr = true })
