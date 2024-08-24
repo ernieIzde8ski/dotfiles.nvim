74ae89d000b64457499ffe1905637ee5d71c4a15
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-08-24 09:41:37 -0400

    Add special `<F6>` binding for `typst` files

---
+++ after/ftplugin/typst.lua
@@ -0,0 +1,33 @@
+local set_keymap = require("helpers.set-keymap")
+
+if vim.g.typst_opts == nil then
+    ---@class typst-opts
+    ---@field filetype "pdf" | "png" | "svg" | nil
+    ---@field target_dir string | nil
+    ---@field open_on_save boolean | nil
+    vim.g.typst_opts = {}
+end
+
+local function compile_file()
+    ---@type string[]
+    local arguments = { "%" }
+    local opts = vim.g.typst_opts
+
+    local target = "%:r." .. (opts.filetype or "pdf")
+    if opts.target_dir ~= nil then
+        target = opts.target_dir .. target
+    end
+    table.insert(arguments, target)
+
+    local command = "!typst c"
+    for _, argument in ipairs(arguments) do
+        command = command .. " '" .. string.gsub(argument, "'", "\\'") .. "'"
+    end
+    vim.cmd(command)
+
+    if opts.open_on_save then
+        vim.cmd("!xdg-open " .. target)
+    end
+end
+
+set_keymap({ "n", "v" }, "<F6>", compile_file, true)
